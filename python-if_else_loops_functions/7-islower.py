a6c1e87b08fb1f266279fd6b2167fa45ab4c9ef7
OmarHasanov <omerhesenov1998@gmail.com>
2026-04-08 07:48:44 +0000

    7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        if i == 8 and j == 9:
-            print("{}{}".format(i, j))
-        else:
-            print("{}{}, ".format(i, j), end="")
+if __name__ == "__main__":
+    for i in range(10):
+        for j in range(i + 1, 10):
+            if i == 8 and j == 9:
+                print("{}{}".format(i, j))
+            else:
+                print("{}{}, ".format(i, j), end="")
