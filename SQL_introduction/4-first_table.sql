21ad6db54ff57c9252c7b9ec0c6ca3cc041be3dc
OmarHasanov <omerhesenov1998@gmail.com>
2026-05-13 12:37:16 +0000

    4

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Creates the table first_table in the current database
+-- Create table with id and name columns if it does not exist
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
